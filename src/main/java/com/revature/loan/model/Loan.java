231bd541d0db57d7c4a489eed4f2d43ba1933690
Pablo Bravo <pablo1221bravo@gmail.com>
2025-02-27 11:31:07 -0600

    Creacion del proyecto y las clases

---
+++ src/main/java/com/revature/loan/model/Loan.java
@@ -0,0 +1,40 @@
+package com.revature.loan.model;
+
+public class Loan {
+    public int id;
+    public int idUser;
+    public int quantity;
+    public String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
